703de9471c6e892e06d130132bc11fd5db03c8a6
notwallas <walter@ibicare.mx>
2026-04-29 15:03:01 -0600

    feat(project-details): enhance project details page with metrics, challenges, and solutions sections

--- src/app/frontend/pages/project-details/project-details.page.ts
+++ src/app/frontend/pages/project-details/project-details.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, ChangeDetectionStrategy, ChangeDetectorRef } from "@angular/core";
+import { Component, OnInit, OnDestroy, inject, ChangeDetectionStrategy, ChangeDetectorRef, NgZone } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ActivatedRoute, RouterModule, Router } from "@angular/router";
 import { NgIcon, provideIcons } from "@ng-icons/core";
@@ -7,7 +7,13 @@ import {
   tablerBrandGithub, 
   tablerExternalLink,
   tablerRocket,
-  tablerCalendar
+  tablerCalendar,
+  tablerAlertTriangle,
+  tablerCircleCheck,
+  tablerActivity,
+  tablerWorld,
+  tablerChevronLeft,
+  tablerChevronRight
 } from "@ng-icons/tabler-icons";
 import cv from "../../../../assets/data.json";
 
@@ -21,27 +27,71 @@ import cv from "../../../../assets/data.json";
     tablerBrandGithub, 
     tablerExternalLink,
     tablerRocket,
-    tablerCalendar
+    tablerCalendar,
+    tablerAlertTriangle,
+    tablerCircleCheck,
+    tablerActivity,
+    tablerWorld,
+    tablerChevronLeft,
+    tablerChevronRight
   })],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProjectDetailsPage implements OnInit {
+export class ProjectDetailsPage implements OnInit, OnDestroy {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private cdr = inject(ChangeDetectorRef);
+  private ngZone = inject(NgZone);
 
   project: any = null;
+  projectIndex: number = 0;
+  private autoPlayInterval: any;
 
   ngOnInit() {
+    window.scrollTo(0, 0);
     const slug = this.route.snapshot.paramMap.get('slug');
     if (slug) {
       this.project = cv.projects.find(p => this.getProjectSlug(p.name) === slug);
       if (!this.project) {
         this.router.navigate(['/cv']);
+        return;
       }
+      this.startAutoPlay();
     } else {
       this.router.navigate(['/cv']);
     }
+    this.cdr.markForCheck();
+  }
+
+  private startAutoPlay() {
+    if (this.project?.images?.length > 1) {
+      this.ngZone.runOutsideAngular(() => {
+        this.autoPlayInterval = setInterval(() => {
+          this.ngZone.run(() => {
+            this.nextImage();
+            this.cdr.markForCheck();
+          });
+        }, 5000);
+      });
+    }
+  }
+
+  nextImage() {
+    if (this.project?.images?.length) {
+      this.projectIndex = (this.projectIndex + 1) % this.project.images.length;
+      this.cdr.markForCheck();
+    }
+  }
+
+  prevImage() {
+    if (this.project?.images?.length) {
+      this.projectIndex = (this.projectIndex - 1 + this.project.images.length) % this.project.images.length;
+      this.cdr.markForCheck();
+    }
+  }
+
+  setProjectIndex(idx: number) {
+    this.projectIndex = idx;
     this.cdr.markForCheck();
   }
 
@@ -51,5 +101,11 @@ export class ProjectDetailsPage implements OnInit {
 
   goBack() {
     this.router.navigate(['/cv'], { fragment: 'PROJECTS' });
+  }
+
+  ngOnDestroy() {
+    if (this.autoPlayInterval) {
+      clearInterval(this.autoPlayInterval);
+    }
   }
 }
