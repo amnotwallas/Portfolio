227fda6b9621186dcc262a98ddcfb272f5161221
notwallas <walter@ibicare.mx>
2026-04-29 22:15:27 -0600

    feat: add view transitions to router for enhanced navigation experience

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/frontend/pages/wrapper/app';
 import { provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter, withHashLocation, withViewTransitions } from '@angular/router';
 import { routes } from './app/frontend/app.routes';
 
 
 bootstrapApplication(App, {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withHashLocation())
+    provideRouter(routes, withHashLocation(), withViewTransitions())
   ]
 })
 .catch((err) => console.error(err));
