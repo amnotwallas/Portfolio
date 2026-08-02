c33156802602a1cbad2954f6a8e71129d8e09739
Walter Jahir Ambriz Reyna <walterardev@gmail.com>
2026-08-01 22:40:13 -0600

    Update production API URL in environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://walter-ai-pbhj.onrender.com/api/v1',
+  apiUrl: 'https://walter-ai-17224146979.us-central1.run.app/api/v1',
   apiKey: 'WALTER_AI_KEY_PLACEHOLDER' 
 };
